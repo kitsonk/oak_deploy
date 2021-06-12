7facce8c5e6b2efcaedc61d17aaf47476d9d86f3
Kitson Kelly <me@kitsonkelly.com>
2021-06-13 07:43:28 +1000

    add explore

---
+++ explore.ts
@@ -0,0 +1,22 @@
+import { Application } from "https://deno.land/x/oak@v7.4.1/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = `<!DOCTYPE html>
+  <html>
+    <head></head>
+    <body>
+      <h1>Explore</h1>
+      <h2>Request Headers</h2>
+      <p><pre>${
+    JSON.stringify([...ctx.request.headers], undefined, "  ")
+  }</pre></p>
+      <h2>Deno.env</h2>
+      <p><pre>${JSON.stringify(Deno.env.toObject(), undefined, "  ")}</pre></p>
+    </body>
+  </html>
+  `;
+});
+
+addEventListener("fetch", app.fetchEventHandler());

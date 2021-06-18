bf94a6005e0e87ff0ca476d8386afdf2b03a7892
Kitson Kelly <me@kitsonkelly.com>
2021-06-18 21:32:49 +1000

    fix content types

--- static.ts
+++ static.ts
@@ -1,10 +1,15 @@
 import { Application, proxy } from "https://deno.land/x/oak@v7.6.1/mod.ts";
+import { lookup } from "https://deno.land/x/media_types@v2.9.0/mod.ts";
 
 const app = new Application();
 
 app.use(proxy(new URL("./static/", import.meta.url), {
   map: {
     "/": "/index.html",
+  },
+  response(res) {
+    res.headers.set("content-type", lookup(res.url) ?? "text/plain");
+    return res;
   },
 }));
 
